4808eb128c3ead89e40f71ada55dbb0a8831c716
Mike Reposa <mreposa@cra.com>
2019-06-12 10:41:45 -0400

    Updated CP3 bootstrap file

--- CP3/bootstrap.sh
+++ CP3/bootstrap.sh
@@ -1,60 +1,68 @@
 #!/usr/bin/env bash
 
+# Configure system for Java
+export JAVA_HOME=/usr/java/jdk1.8.0_211
+export PATH=$PATH:$JAVA_HOME/bin
+update-alternatives --install "/usr/bin/java" "java" "/usr/java/jdk1.8.0_211/bin/java" 1
+update-alternatives --install "/usr/bin/javac" "javac" "/usr/java/jdk1.8.0_211/bin/javac" 1
+update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/java/jdk1.8.0_211/bin/javaws" 1
+update-alternatives --set java /usr/java/jdk1.8.0_211/bin/java
+update-alternatives --set javac /usr/java/jdk1.8.0_211/bin/javac
+update-alternatives --set javaws /usr/java/jdk1.8.0_211/bin/javaws
+
 # Upgrade the package installer
-sudo apt-get -y upgrade
+apt-get -y upgrade
 
 # Update package manager
-sudo apt-get update
+apt-get update
 
 # Common Packages
-sudo apt-get install -y build-essential tcl tk --no-install-recommends
-sudo apt-get install -y libpq-dev vim --no-install-recommends
-sudo apt-get install -y software-properties-common python-software-properties
+apt-get install -y build-essential tcl tk --no-install-recommends
+apt-get install -y libpq-dev vim --no-install-recommends
+apt-get install -y software-properties-common python-software-properties
+apt-get install -f -y
 
-# Oracle Java 8
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo add-apt-repository ppa:webupd8team/java -y
-sudo apt-get update
-sudo apt-get install oracle-java8-installer
-sudo apt-get install -y oracle-java8-set-default
 
 # Scala 2.12.2
 wget www.scala-lang.org/files/archive/scala-2.12.2.deb
-sudo dpkg -i scala-2.12.2.deb
+dpkg -i scala-2.12.2.deb
 
 # SBT 0.13.15
 wget -c https://bintray.com/artifact/download/sbt/debian/sbt-0.13.15.deb
-sudo dpkg -i sbt-0.13.15.deb
-sudo apt-get update
-sudo apt-get install sbt
+dpkg -i sbt-0.13.15.deb
+apt-get update
+apt-get install sbt
+apt-get install -f -y
 
 # jq
-sudo apt-get install -y jq
-sudo apt-get update
+apt-get install -y jq
+apt-get update
+apt-get install -f -y
 
 # Octave & GNUPlot
-sudo add-apt-repository -y ppa:octave/stable
-sudo apt-get update
-sudo apt-get -y install octave liboctave-dev gnuplot --no-install-recommends
+add-apt-repository -y ppa:octave/stable
+apt-get update
+apt-get -y install octave liboctave-dev gnuplot --no-install-recommends
+apt-get install -f -y
 
 # Apache ActiveMQ 5.14.0
 wget https://archive.apache.org/dist/activemq/5.14.0/apache-activemq-5.14.0-bin.tar.gz
-tar -C /home/ubuntu -zxvf apache-activemq-5.14.0-bin.tar.gz
+tar -C /cp3 -zxvf apache-activemq-5.14.0-bin.tar.gz
 
 # DOS2Unix
-# sudo apt-get install -y dos2unix
+# apt-get install -y dos2unix
 
 # Cleanup Install Artifacts
-sudo rm -f scala-2.12.2.deb
-sudo rm -f sbt-0.13.15.deb
-sudo rm -f apache-activemq-5.14.0-bin.tar.gz
+rm -f scala-2.12.2.deb
+rm -f sbt-0.13.15.deb
+rm -f apache-activemq-5.14.0-bin.tar.gz
 
 # Clean up previous logs / scenarios
-sudo rm -f princess.log
-sudo rm -f scenario.json
+rm -f princess.log
+rm -f scenario.json
 
 # Access Control Settings
-sudo chmod -R 777 ./apache-activemq-5.14.0
-sudo chmod 777 ./start
-sudo chmod 777 ./grizzly-service.sh
-sudo chmod 777 ./run
+chmod -R 777 ./apache-activemq-5.14.0
+chmod 777 ./start
+chmod 777 ./grizzly-service.sh
+chmod 777 ./run
